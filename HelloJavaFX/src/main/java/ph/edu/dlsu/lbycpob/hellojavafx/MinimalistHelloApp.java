0445824774b511d8b1cdb423c5851d754e53d8ab
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-08-11 18:29:54 +0800

    Add MinimalistHelloApp.java

--- HelloJavaFX/src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/MinimalistHelloApp.java
+++ HelloJavaFX/src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/MinimalistHelloApp.java
@@ -1,4 +1,28 @@
 package ph.edu.dlsu.lbycpob.hellojavafx;
 
-public class MinimalistHelloApp {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MinimalistHelloApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello LBYCPOB! by Abuel and Malirong");
+
+        label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        label.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(label, 320, 120);
+
+        stage.setTitle("Minimalist Hello");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
