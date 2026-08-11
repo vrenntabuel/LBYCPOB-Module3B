2467cd90f5385fd5f0c16d6e3c9a709a74384c17
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-08-11 17:24:58 +0800

    Delete HelloJavaFX directory

--- HelloJavaFX/src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package ph.edu.dlsu.lbycpob.hellojavafx;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
