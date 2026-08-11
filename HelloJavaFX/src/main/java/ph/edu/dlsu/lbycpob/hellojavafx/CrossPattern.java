78e6b60e5616dc0819640f56741c0fcc9b34e30e
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-08-11 20:32:17 +0800

    Added CrossPattern.java to run cross.fxml

---
+++ HelloJavaFX/src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/CrossPattern.java
@@ -0,0 +1,32 @@
+package ph.edu.dlsu.lbycpob.hellojavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CrossPattern extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                        "/ph/edu/dlsu/lbycpob/hellojavafx/cross.fxml"
+                )
+        );
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Cross Pattern");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
